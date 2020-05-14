980329192dfc1e15dd0d2981764577acd5b98f87
Glenn Butcher <glenn.butcher@gmail.com>
2020-05-14 09:15:01 -0600

    more dev

--- ssftool.cpp
+++ ssftool.cpp
@@ -1,5 +1,6 @@
 
 //#include <stdio.h>
+#include <stdarg.h> 
 #include <iostream>
 #include <string>
 #include <vector>
@@ -9,6 +10,11 @@
 struct ssfdata {
 	unsigned w; 
 	float r, g, b;
+};
+
+struct channeldata {
+	unsigned p;
+	float v;
 };
 
 // Helpers:
@@ -62,6 +68,28 @@ std::vector<std::string> bifurcate(std::string strg, char c = ' ', bool fromback
 	return result;
 }
 
+std::string string_format(const std::string fmt, ...) 
+{
+    int size = ((int)fmt.size()) * 2 + 50;   // Use a rubric appropriate for your code
+    std::string str;
+    va_list ap;
+    while (1) {     // Maximum two passes on a POSIX system...
+        str.resize(size);
+        va_start(ap, fmt);
+        int n = vsnprintf((char *)str.data(), size, fmt.c_str(), ap);
+        va_end(ap);
+        if (n > -1 && n < size) {  // Everything worked
+            str.resize(n);
+            return str;
+        }
+        if (n > -1)  // Needed size returned
+            size = n + 1;   // For null char
+        else
+            size *= 2;      // Guess at a larger size (OS specific)
+    }
+    return str;
+}
+
 void err(std::string msg)
 {
 	std::cout << msg << std::endl;
@@ -97,17 +125,33 @@ std::vector<ssfdata> getData(std::vector<std::string> lines)
 	return data;
 }
 
+std::vector<channeldata> channelMaxes(std::vector<ssfdata> data)
+{
+	std::vector<channeldata> max;
+	channeldata rmax = {0,0.0}, gmax= {0,0.0}, bmax= {0,0.0};
+	for (unsigned i=0; i<data.size(); i++) {
+		if (data[i].r > rmax.v) {rmax.v = data[i].r; rmax.p =  i;}
+		if (data[i].g > gmax.v) {gmax.v = data[i].g; gmax.p =  i;}
+		if (data[i].b > bmax.v) {bmax.v = data[i].b; bmax.p =  i;}
+	}
+	max.push_back(rmax);
+	max.push_back(gmax);
+	max.push_back(bmax);
+	return max;
+}
+
 std::vector<std::string> channel_extract(std::vector<std::string> lines)
 {
 	std::vector<std::string> l;
 	for (std::vector<std::string>::iterator line = lines.begin(); line !=lines.end(); ++line)
 		if ((*line).find("red") == 0 | (*line).find("green") == 0 | (*line).find("blue") == 0)
-			printf("%s\n",(*line).c_str());
+			l.push_back(string_format("%s",(*line).c_str()));
 	return l;
 }
 
-std::map<std::string, std::vector<std::string>> data_transpose(std::vector<std::string> lines)
+std::vector<std::string> data_transpose(std::vector<std::string> lines)
 {
+	std::vector<std::string> l;
 	std::map<std::string, std::vector<std::string>> data;
 	for (std::vector<std::string>::iterator line = lines.begin(); line !=lines.end(); ++line) {
 		std::vector<std::string> linedata = split(*line, ",");
@@ -115,7 +159,9 @@ std::map<std::string, std::vector<std::string>> data_transpose(std::vector<std::
 		linedata.erase(linedata.begin());
 		data[key] = linedata;
 	}
-	return data;
+	for (unsigned i = 0; i < data["red"].size(); i++)
+		l.push_back(string_format("%d,%s,%s,%s", i, data["red"][i].c_str(), data["green"][i].c_str(), data["blue"][i].c_str())); 
+	return l;
 }
 
 
@@ -138,20 +184,44 @@ void ssf_extract(FILE *f)
 
 void ssf_transpose(FILE *f)
 {
-	
 	std::vector<std::string> lines = getFile(f);
-	std::map<std::string, std::vector<std::string>> data = data_transpose(lines);
-	for (unsigned i = 0; i < data["red"].size(); i++)
-		printf("%d,%s,%s,%s\n", i, data["red"][i].c_str(), data["green"][i].c_str(), data["blue"][i].c_str()); 
+	lines = data_transpose(lines);
+	for (std::vector<std::string>::iterator line = lines.begin(); line !=lines.end(); ++line)
+		printf("%s\n",(*line).c_str());
+}
+
+void ssf_channelmaxes(FILE *f)
+{
+	std::vector<std::string> lines = getFile(f);
+	std::vector<ssfdata> data = getData(lines);
+	std::vector<channeldata> max =  channelMaxes(data);
+	printf("red:%f,%d;green:%f,%d;blue:%f,%d\n", max[0].v, max[0].p, max[1].v, max[1].p, max[2].v, max[2].p);
 }
 
 void ssf_wavelengthcalibrate(FILE *f, std::string calibrationfile, int redwavelength, int greenwavelength, int bluewavelength=0)
 {
 	FILE *c = fopen(calibrationfile.c_str(), "r");
-	if (c == NULL) err("Error: calibration file not found.");
-	std::vector<ssfdata> calib = getData(getFile(c));
+	if (c == NULL) err(string_format("Error: calibration file %s not found.",calibrationfile.c_str()));
+	std::vector<std::string> caliblines = getFile(c);
+	caliblines = channel_extract(caliblines);
+	caliblines = data_transpose(caliblines);
+	std::vector<ssfdata> calibdata = getData(caliblines);
+	std::vector<channeldata> max =  channelMaxes(calibdata);
 	
-	std::vector<ssfdata> data = getData(getFile(f));
+	unsigned nchannels = 0;
+	if (redwavelength != 0) nchannels++;
+	if (greenwavelength != 0) nchannels++;
+	if (bluewavelength != 0) nchannels++;
+	if (nchannels < 2) err("Error: need at least two channels");
+	
+	std::vector<ssfdata> specdata = getData(getFile(f));
+	
+	if (redwavelength != 0) specdata[max[0].p].w = redwavelength;
+	if (greenwavelength != 0) specdata[max[1].p].w = greenwavelength;
+	if (bluewavelength != 0) specdata[max[2].p].w = bluewavelength;
+	
+	for (std::vector<ssfdata>::iterator dat = specdata.begin(); dat !=specdata.end(); ++dat)
+		printf("%d,%f,%f,%f\n", (*dat).w, (*dat).r, (*dat).g, (*dat).b);
 	
 }
 
@@ -160,22 +230,42 @@ int main(int argc, char ** argv)
 {
 	FILE *f;
 
-	if (argc < 2) err("Usage: ssftool <operation> [<datafile>] [parameters...]"); 
+	if (argc <= 1) err("Usage: ssftool <operation> [<datafile>] [parameters...]"); 
 	std::string operation = std::string(argv[1]);
+	bool usefile;
 	
-	if (argc < 3)
+	if (argc <= 2) {
 		f = stdin;
-	else
+		usefile = false;
+	}
+	else {
 		f = fopen(argv[2], "r"); 
+		usefile = true;
+	}
 	
 	if (f == NULL) err("file not found.");
 	
-	if (operation == "list")
+	if (operation == "list") {
 		ssf_list(f);
-	else if (operation == "extract")
+	}
+	else if (operation == "extract") {
 		ssf_extract(f);
-	else if (operation == "transpose")
+	}
+	else if (operation == "transpose") {
 		ssf_transpose(f);
+	}
+	else if (operation == "channelmaxes") {
+		ssf_channelmaxes(f);
+	}
+	else if (operation == "wavelengthcalibrate") {
+		std::string calibfile;
+		if (usefile)
+			calibfile = std::string(argv[3]);
+		else
+			calibfile = std::string(argv[2]);
+
+		ssf_wavelengthcalibrate(f, calibfile, 300, 200, 100);
+	}
 	else printf("Error: unrecognized operation.");
 	
 	fclose(f);
