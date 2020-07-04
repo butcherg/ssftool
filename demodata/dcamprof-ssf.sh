709988fbc630fb33c5d3c6c06c11f1c4eb046f92
Glenn Butcher <glenn.butcher@gmail.com>
2020-07-03 20:48:27 -0600

    added dcamprof-ssf.sh

---
+++ demodata/dcamprof-ssf.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+description='(none)'
+copyright='(none)'
+
+while getopts n:c:h: option
+do
+case "${option}"
+in
+n) description=${OPTARG};;
+c) copyright=${OPTARG};;
+h)  echo "Usage: $ ./dcamprof-ssf.sh [-n cameraname] [-c copyright] <jsonfile>.json"
+    exit
+    ;;
+esac
+done
+shift $(($OPTIND -1))
+
+if ! [[ $@ =~ \.json$ ]]; then
+	echo "$@ is an invalid filename (extension is not .json)"
+	exit
+fi
+
+fbname=$(basename "$@" '.json')
+
+dcamprof make-target -c $@ -p cc24 _target.ti3
+dcamprof make-profile -c $@ _target.ti3 _profile.json
+dcamprof make-icc -n "\"$description\"" -c "\"$copyright\"" -p xyzlut _profile.json $fbname.icc
+rm _*
