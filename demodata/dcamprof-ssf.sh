61f25c42a22894d28253ac6cc9c761979bb9140f
Glenn Butcher <glenn.butcher@gmail.com>
2020-08-10 10:50:10 -0600

    added echos to script for progression

--- demodata/dcamprof-ssf.sh
+++ demodata/dcamprof-ssf.sh
@@ -25,7 +25,13 @@ fi
 
 fbname=$(basename "$@" '.json')
 
+echo
+echo "dcamprof make-target -c $@ -p cc24 _target.ti3"
 dcamprof make-target -c $@ -p cc24 _target.ti3
+echo
+echo "dcamprof make-profile -c $@ _target.ti3 _profile.json"
 dcamprof make-profile -c $@ _target.ti3 _profile.json
+echo
+echo "dcamprof make-icc -n "\"$description\"" -c "\"$copyright\"" -p xyzlut _profile.json $fbname.icc"
 dcamprof make-icc -n "\"$description\"" -c "\"$copyright\"" -p xyzlut _profile.json $fbname.icc
 rm _*
