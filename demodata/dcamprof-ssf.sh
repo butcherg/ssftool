be47cb50165964f6ba67f82a27de094b77b0f5da
Glenn Butcher <glenn.butcher@gmail.com>
2020-08-12 11:00:45 -0600

    dcamprof-ssh.sh -r switch to do reports

--- demodata/dcamprof-ssf.sh
+++ demodata/dcamprof-ssf.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+if [[ $# == 0 ]]; then
+	echo "Options:"
+	echo "	-c <copyright>   - inserts copyright statement in the icc file"
+	echo "	-n <description> - inserts description in the icc file"
+	echo "	-r               - adds the \"-r reports\" switch for dcamprof make-profile using the supplied report directory"
+	exit
+fi
+
 set -e
 
 description='(none)'
 copyright='(none)'
 
-while getopts n:c:h: option
+while getopts n:c:h:r option
 do
 case "${option}"
 in
@@ -14,6 +22,7 @@ c) copyright=${OPTARG};;
 h)  echo "Usage: $ ./dcamprof-ssf.sh [-n cameraname] [-c copyright] <jsonfile>.json"
     exit
     ;;
+r) reports="-r reports";;
 esac
 done
 shift $(($OPTIND -1))
@@ -29,8 +38,8 @@ echo
 echo "dcamprof make-target -c $@ -p cc24 _target.ti3"
 dcamprof make-target -c $@ -p cc24 _target.ti3
 echo
-echo "dcamprof make-profile -c $@ _target.ti3 _profile.json"
-dcamprof make-profile -c $@ _target.ti3 _profile.json
+echo "dcamprof make-profile $reports -c $@ _target.ti3 _profile.json"
+dcamprof make-profile $reports -c $@ _target.ti3 _profile.json
 echo
 echo "dcamprof make-icc -n "\"$description\"" -c "\"$copyright\"" -p xyzlut _profile.json $fbname.icc"
 dcamprof make-icc -n "\"$description\"" -c "\"$copyright\"" -p xyzlut _profile.json $fbname.icc
