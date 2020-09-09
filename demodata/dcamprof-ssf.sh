9c5ff093e931532cd3a0de7797be770dea2da6da
Glenn Butcher <glenn.butcher@gmail.com>
2020-09-09 08:30:28 -0600

    reorganized dcamprof-ssf.sh

--- demodata/dcamprof-ssf.sh
+++ demodata/dcamprof-ssf.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 if [[ $# == 0 ]]; then
-	echo "dcamprof-ssf.sh [-cnr] <ssf.json>
+	echo "dcamprof-ssf.sh [-cnr] <ssf.json>"
 	echo "Options:"
 	echo "	-c <copyright>   - inserts copyright statement in the icc file"
 	echo "	-n <description> - inserts description in the icc file"
-	echo "	-r               - adds the \"-r reports\" switch for dcamprof make-profile using the supplied report directory"
+	echo "	-r               - enables report generation for dcamprof make-profile"
+	echo "  -s <spectra>     - uses the specified spectra (default: cc24)"
+	echo "  -f <filespec>	 - specifies a filespec to use for the product filenames instead of the input filespec"
 	exit
 fi
 
@@ -13,8 +15,14 @@ set -e
 
 description='(none)'
 copyright='(none)'
+refspectra='cc24'
 
-while getopts n:c:h:r option
+#load configuration file, if present:
+if test -f "dcamprof-ssf.conf"; then
+        source dcamprof-ssf.conf
+fi
+
+while getopts n:c:h:f:r option
 do
 case "${option}"
 in
@@ -23,7 +31,9 @@ c) copyright=${OPTARG};;
 h)  echo "Usage: $ ./dcamprof-ssf.sh [-n cameraname] [-c copyright] <jsonfile>.json"
     exit
     ;;
-r) reports="-r reports";;
+r) reportflag="-r";;
+s) refspectra=${OPTARG};;
+f) fbaltname=${OPTARG};;
 esac
 done
 shift $(($OPTIND -1))
@@ -33,14 +43,22 @@ if ! [[ $@ =~ \.json$ ]]; then
 	exit
 fi
 
-fbname=$(basename "$@" '.json')
+if [ -z ${fbaltname+x} ]; then
+	fbname=$(basename "$@" '.json')
+else
+	fbname=$fbaltname
+fi
+
+if [ -z ${reportflag+x} ]; then
+	reportflag="-r $fbname-reports"
+fi
 
 echo
-echo "dcamprof make-target -c $@ -p cc24 _target.ti3"
-dcamprof make-target -c $@ -p cc24 _target.ti3
+echo "dcamprof make-target -c $@ -p $refspectra _target.ti3"
+dcamprof make-target -c $@ -p $refspectra _target.ti3
 echo
-echo "dcamprof make-profile $reports -c $@ _target.ti3 _profile.json"
-dcamprof make-profile $reports -c $@ _target.ti3 _profile.json
+echo "dcamprof make-profile $reportflag $fbname-reports  -c $@ _target.ti3 _profile.json"
+dcamprof make-profile $reportflag -c $@ _target.ti3 _profile.json
 echo
 echo "dcamprof make-icc -n \"$description\" -c \"$copyright\" -p xyzlut _profile.json $fbname.icc"
 dcamprof make-icc -n "$description" -c "$copyright" -p xyzlut _profile.json $fbname.icc
